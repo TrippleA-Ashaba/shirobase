0d3727caf356879efbe9453cbf78e7bdd07f4f2c
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2025-11-21 22:23:18 +0300

    Add accounts and users apps with authentication features, including registration, login, and profile management. Update dependencies in pyproject.toml and configure middleware and settings for CSRF and permissions.

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,9 +14,45 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.decorators import login_not_required
+from django.urls import include, path, re_path
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+
+@method_decorator(login_not_required, name="dispatch")
+class IndexView(TemplateView):
+    template_name = "index.html"
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # ============================ Spectacular API documentation ===============================
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    # ============================ Admin ===============================
+    path("admin/", admin.site.urls),
+    # ============================ All Auth URLs ================================================
+    path("api/accounts/", include("apps.accounts.urls", namespace="accounts")),
+    # For email verification
+    path("api/accounts/", include("allauth.urls")),
+    # ============================ Frontend URLs ================================================
+    # # Keep at the bottom such that all other routes are handled by the frontend
+    path("", IndexView.as_view(), name="index"),
+    re_path(r"^(?P<path>.*)$", IndexView.as_view(), name="index"),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+        path("__reload__/", include("django_browser_reload.urls")),
+    ] + urlpatterns
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
